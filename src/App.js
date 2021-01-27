119385ac73eb6ee5bbafee75954591dc1f0cce99
endorphin82 <endorphin82@gmail.com>
2021-01-27 17:14:54 +0200

    googleId

--- src/App.js
+++ src/App.js
@@ -2,45 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import {GoogleLogin} from "react-google-login";
 import axios from "axios";
-// import * as querystring from "querystring";
-// import * as url from "url";
-import {stringify} from "qs";
+// import LinkedIn from "react-linkedin-login/src";
+// import SocialButton from './SocialButton'
 
 
-console.log("v2.2.44")
+console.log("v2.2.55")
+const linkedinError = (err) => {
+    console.log("err", err);
+}
+const linkedinResponse = (res) => {
+    console.log("res", res);
+    // axios.post('https://endorphin.fun/auth/google/callback', null,
+    //     {
+    //         params: {
+    //             serviceId: res.accessToken,
+    //             service: res.tokenObj.id_token
+    //         }
+    //     }
+    // )
+    /*
+                axios.post('https://endorphin.fun/auth/google/callback',
+                    {
+                        access_token: res.accesstoken,
+                    }
+        )
+        console.log(res);
+        return await res.status;
+    */
+
+    //     .then(r => {
+    //     console.log(r);
+    // }).catch(e => {
+    //     linkedinError(e)
+    // })
+}
 
 const googleResponse = (response) => {
-    // const tokenBlob = new Blob([JSON.stringify({
-    //     access_token: response.accessToken,
-    //     email: response.profileObj.email,
-    //     name: response.profileObj?.name,
-    //     avatar: response.profileObj?.imageUrl,
-    //     googleId: response.googleId,
-    //     id_token: response.tokenObj.id_token
-    // }, null, 2)], {type: 'application/json'});
-
-    // const tokenBlob = new Blob([JSON.stringify({
-    //     serviceId: response.accessToken,
-    //     service: response.tokenObj.id_token
-    // }, null, 2)], {type: 'application/json'});
-
-    // const options = {
-    //     method: 'POST',
-    //     body: tokenBlob,
-    //     // body: new Blob([JSON.stringify(response, null, 2)], {type: 'application/json'}),
-    //     mode: 'no-cors',
-    //     cache: 'default'
-    // };
-
-    // const options = {
-    //     headers: {'Access-Control-Allow-Origin': '*'},
-    //     method: 'POST',
-    //     url: 'https://endorphin.fun/auth/google/callback',
-    //     body: tokenBlob,
-    //     // body: new Blob([JSON.stringify(response, null, 2)], {type: 'application/json'}),
-    //     mode: 'cors',
-    //     cache: 'default'
-    // };
 
     console.log("response", response);
     // fetch('https://endorphin.fun/auth/google/callback', options).then(r => {
@@ -71,21 +68,21 @@ const googleResponse = (response) => {
     //     serviceId: response.accessToken,
     //     service: response.tokenObj.id_token
     // })
-    axios.post('https://endorphin.fun/auth/google/callback',
-        // "serviceId=sdfs&service=sdfs"
-        // params.toString()
-        stringify({
-            serviceId: response.accessToken,
-            service: response.tokenObj.id_token
-        })
+
+    /*    axios.post('https://endorphin.fun/auth/google/callback', null,
+            {
+                params: {
+                    serviceId: response.accessToken,
+                    service: response.tokenObj.id_token
+                }
+            }
+        )*/
+    axios.post('https://con29.tk:8080/rest-auth/google',
+        {
+            access_token: response.accessToken
+        }
     ).then(r => {
-        console.log(r);
-        // const token = r.headers.get('x-auth-token');
-        // r.json().then(user => {
-        //     if (token) {
-        //         this.setState({isAuthenticated: true, user, token})
-        //     }
-        // });
+        console.log("then", r);
     })
 
 };
@@ -98,6 +95,15 @@ function App() {
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
+                {/*       <SocialButton
+                    provider='google'
+                    appId='319511837370-6h11919mja93u8rijnlbu3h5j8kt9k5l'
+                    onLoginSuccess={googleResponse}
+                    triggerLogin={googleResponse}
+                    onLoginFailure={linkedinError}
+                >
+                    Login with Google
+                </SocialButton>*/}
                 <GoogleLogin
                     clientId="319511837370-6h11919mja93u8rijnlbu3h5j8kt9k5l.apps.googleusercontent.com"
                     buttonText="Login"
@@ -106,6 +112,14 @@ function App() {
                     // uxMode="popup"
                     // redirectUri="https://endorphin.fun/google/redirect"
                 />
+
+                <br/>
+                {/*<LinkedIn*/}
+                {/*    clientId='78bmlnwju2j0vj'*/}
+                {/*    callback={linkedinResponse}*/}
+                {/*    text='LinkedIn'/>*/}
+
+
             </header>
         </div>
     );
